e037c6ee9fdb783f2b42e3f055877158f0ed13b2
Ulfric Irons <ironsulfric@gmail.com>
2026-06-16 10:39:42 +0100

    Adjusting browser settings

--- Core/DriverFactory.cs
+++ Core/DriverFactory.cs
@@ -19,11 +19,25 @@ public static class DriverFactory
     {
         var options = new ChromeOptions();
         if (headless) options.AddArgument("--headless=new");
+
+        // Performance optimizations
+        options.AddArgument("--no-sandbox");
+        options.AddArgument("--disable-dev-shm-usage");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--no-first-run");
+        options.AddArgument("--no-default-browser-check");
+        options.AddArgument("--disable-default-apps");
+        options.AddArgument("--disable-sync");
+        options.AddArgument("--disable-extensions");
+        options.AddArgument("--disable-web-resources");
+
+        // Disable notifications and password features
         options.AddArgument("--disable-notifications");
         options.AddArgument("--disable-save-password-bubble");
         options.AddArgument("--disable-password-manager-reauthentication");
         options.AddArgument("--disable-autofill-keyboard-accessory-view");
         options.AddArgument("--disable-component-extensions-with-background-pages");
+
         options.AddUserProfilePreference("profile.password_manager_enabled", false);
         options.AddUserProfilePreference("credentials_enable_service", false);
         return options;
@@ -32,12 +46,26 @@ public static class DriverFactory
     {
         var options = new EdgeOptions();
         if (headless) options.AddArgument("--headless=new");
+
+        // Performance optimizations
+        options.AddArgument("--no-sandbox");
+        options.AddArgument("--disable-dev-shm-usage");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--disable-extensions");
+        options.AddArgument("--disable-sync");
+
         return options;
     }
     private static FirefoxOptions CreateFirefoxOptions(bool headless)
     {
         var options = new FirefoxOptions();
         if (headless) options.AddArgument("-headless");
+
+        // Performance optimizations
+        options.SetPreference("dom.max_script_run_time", 30);
+        options.SetPreference("browser.sessionstore.max_tabs_undo", 0);
+        options.SetPreference("privacy.trackingprotection.enabled", false);
+
         return options;
     }
 }
